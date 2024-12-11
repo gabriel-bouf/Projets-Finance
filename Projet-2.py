046c32044989c33351f00c2ab2333efd31b1231b
Gabriel Bouf <165051839+gabriel-bouf@users.noreply.github.com>
2024-12-11 17:31:24 +0100

    Update Projet-2.py

--- Projet-2.py
+++ Projet-2.py
@@ -2,91 +2,86 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
-from math import sqrt
+from math import sqrt, exp,log
 from scipy.optimize import minimize
-import requests
-from mistralai.client import MistralClient
-from mistralai.models.chat_completion import ChatMessage
+from mistralai import Mistral
+#from mistralai.models import ChatMessage
 import json
 
+def constraints(weights):
+    return np.sum(weights)-1 #contrainte=0 dans minimise; ici on veut 100% du portefeuille en actions
 
-def contrainte(poids):
-    return np.sum(poids)-1 #contrainte=0 dans minimise; ici on veut 100% du portefeuille en actions
+def average_daily_portfolio_infos(weights:list,returns:pd.DataFrame):
+    # [portfolio_return,portfolio_risk]
+    return [np.dot(weights, returns.mean()),sqrt(np.dot(weights, np.dot(returns.cov(), weights)))]
 
-def calcul_rendement_portefeuille(poids, rendements_moyens):
-    return np.dot(poids, rendements_moyens)*252
+def sharpe_ratio(weights:list,returns: pd.DataFrame):
+    daily_risk_free_rate=exp(log(1+0.03)/252)-1
+    # constant = 3% over a year (252 days)
+    p_return=average_daily_portfolio_infos(weights,returns)[0]
+    p_risk=average_daily_portfolio_infos(weights,returns)[1]
+    return (p_return-daily_risk_free_rate)/p_risk
 
-def calcul_risque_portefeuille(poids, matrice_cov):
-    return sqrt(np.dot(poids, np.dot(matrice_cov, poids))*252)
-
-def perfomance_portefeuille(poids:list,rendements: pd.DataFrame):
-    #note la performance du portefeuille selon une fonction (ici le ratio risque/rendement)
-    matrice_cov= rendements.cov()
-    rendements_moyens=rendements.mean()
-    rendement_attendu_portfolio=calcul_rendement_portefeuille(poids,rendements_moyens)
-    portfolio_risk=calcul_risque_portefeuille(poids, matrice_cov)
-    #ici mettre la fonction qu'on veut, en fonction de comment on aime le risque
-    #du plus au moins risqué: risque/rendement**2 , risque/rendement ,  risque, etc
-    return portfolio_risk/(rendement_attendu_portfolio)
-
-def meilleure_combinaison(actions:list,start:str, end:str):
-    #trouve la meilleure combinaison d'actions pour un portefeuille
-    historiques=[]
-    m=len(actions)
-    for i in range(m):
-        historiques.append(yf.Ticker(actions[i]).history(start=start, end=end).Close)#récolte des historiques des actions à la fermeture
-    df_histo=pd.DataFrame(historiques).T #pour transposer
-    df_histo.columns=actions
-    rendements= df_histo.pct_change().dropna()#calcul des rendements
-    bounds = [(0, 1) for k in range(m)]#proportion de chaque action entre 0 et 1
-    ci=[1/m for k in range(m)]#poids initial de chaque action égaux
-
-    contraintes={'type': 'eq', 'fun': contrainte}
-    meilleure_combi =minimize(perfomance_portefeuille,ci,args=(rendements), method='SLSQP', bounds=bounds, constraints=contraintes)
-    return meilleure_combi.x, rendements
-
-def generer_portefeuilles(rendements, nombre_portefeuilles):
-    nombre_actions = rendements.shape[1]
-    rendements_moyens = rendements.mean()
-    matrice_cov = rendements.cov()
-    couples_rendement_risque = []
-
-    for _ in range(nombre_portefeuilles):
-        #poids totalisent 1
-        poids = np.random.random(nombre_actions)
-        poids /= np.sum(poids)
-
-        rendement = calcul_rendement_portefeuille(poids, rendements_moyens)
-        risque = calcul_risque_portefeuille(poids, matrice_cov)
-        couples_rendement_risque.append((rendement, risque))
-    return couples_rendement_risque
-
-def afficher_portefeuilles(couples_rendement_risque,meilleur_couple):
-    rendements, risques = zip(*couples_rendement_risque)#* pour décompresser
-    rendements = np.array(rendements)
-    risques = np.array(risques)
-    plt.figure(figsize=(10, 10))
-    # Scatter plot avec le ratio rendement/risque
-    plt.scatter(risques, rendements, c=rendements/risques, marker='o',s=5)
-    plt.scatter(meilleur_couple[1], meilleur_couple[0], color='red', marker='o', s=30)
-    #meilleur_couple[1] = risque
-    plt.title('Frontière efficiente des portefeuilles\nie toutes les performances possibles des portefeuilles pour les actions choisies\nLe point rouge est le portefeuille optimal')
-    plt.xlabel('Risque')
-    plt.ylabel('Rendement attendu')
-    plt.colorbar(label='Rendement/Risque')
-    plt.show()
-
-def tickers_mistral(n,start,end):
-    #demande à l'API Mistral n tickers d'entreprises de secteurs variés ayant performé entre start et end
-    prefixe="Il faut que tu varie tes réponse. Si tu choisi de mettre l'un ou plusieurs d'entre eux, met juste le bon ticker : change 'FB' par 'META', change 'BNP' par 'BNP.PA',change 'TOT' par 'TTE.PA',change 'BT' par 'BT-A.L'. Ne porpose pas 'BNSF' ni 'TWTR'. N'ajoute aucuns détails ou explication, n'invente aucun ticker. Ne met pas de retour à la ligne dans la liste."
+def minimizer_sharpe_ratio(weights:list,returns:pd.DataFrame):
+    return -sharpe_ratio(weights,returns)
     
-    api_key = "a remplir"
+def optimizator(tickers:list,start,end):
+    returns=yf.Tickers(tickers).history(start=start,end=end).pct_change(fill_method=None)['Close']
+    initial_weights=[1/len(tickers) for elt in tickers]
+    bounds = [(0, 1) for elt in tickers]
+    c={'type': 'eq', 'fun': constraints}
+    x0=np.array(initial_weights)
+    opti=minimize(minimizer_sharpe_ratio,x0,args=returns,bounds=bounds,constraints=c)
+    #opti.x is a np tab
+    return opti.x.tolist(), returns
 
-    if api_key == "a remplir":
-        print("Vous devez rentrer votre clé API pour utiliser Mistral")
-        #Si vous n'avez pas de clé API, les actions sont choisies aléatoirement parmi cette liste :
-        
-        base_de_donnee_actions= [
+def random_portfolio(returns,n):
+    n_actions = returns.shape[1]
+    couples=[]
+    for i in range(n):
+        weight = np.random.random(n_actions)
+        weight /= np.sum(weight)
+        couples.append(average_daily_portfolio_infos(weight,returns))
+    return couples
+
+def print_portfolios(couples,optimal_portfolio):
+    returns, risks = zip(*couples)#* pour décompresser
+    returns = np.array(returns)
+    risks = np.array(risks)
+    daily_risk_free_rate=exp(log(1+0.03)/252)-1
+    # constant = 3% over a year (252 days)
+
+    plt.figure(figsize=(10, 10))
+    plt.scatter(risks, returns, c=(returns-daily_risk_free_rate)/risks, marker='o',s=5)
+    plt.scatter(optimal_portfolio[1], optimal_portfolio[0], color='red', marker='o', s=30)
+    plt.title('Efficient portfolio frontier\nOptimal portfolio in red')
+    plt.xlabel('Risk')
+    plt.ylabel('Daily expected return')
+    plt.colorbar(label='Sharpe ratio')
+    plt.show()    
+
+def global_print(tickers,start,end,n_portfolio):
+    opti_weights,returns=optimizator(tickers,start,end)
+    opti_weights= [round(x,3) for x in opti_weights]
+    filtered_opti_set =[(tickers[i], opti_weights[i]) for i in range(len(opti_weights)) if opti_weights[i] > 0]
+    deleted_opti_set=[tickers[i] for i in range(len(opti_weights)) if opti_weights[i] == 0]
+    
+    opti_couple=average_daily_portfolio_infos(opti_weights,returns)
+    print("Optimal set :\n",filtered_opti_set)
+    print("\n Deleted tickers : \n",deleted_opti_set)
+    print(f'This portfolio is supposed to have a daily return of {round(opti_couple[0]*100,4)}% and a daily risk of {round(opti_couple[1]*100,4)}% based on {start} to {end}')
+    couples = random_portfolio(returns, n_portfolio)
+    print_portfolios(couples,opti_couple)
+    return opti_weights,opti_couple[0]
+
+def mistral_tickers_selection(n_tickers):
+    #Mistral AI selects n tickers of diverse firms which had performed between start and end
+    #but you need an api key
+    api_key = ""
+    if api_key == "":
+        print("You need an api key to use Mistral")
+        #If you don't have an API key, tickers are randomly choose based on this database :
+        tickers_database= [
         "AAPL", "MSFT", "GOOGL", "ORCL", "IBM",     # Tech
         "JNJ", "PFE", "MRK", "UNH", "ABT",           # Santé
         "JPM", "BAC", "GS", "C", "MS",               # Services financiers
@@ -99,47 +94,68 @@ def tickers_mistral(n,start,end):
         "BA", "CAT", "GE", "DE", "LMT",              # Industriels
         "NFLX", "DIS", "T", "VZ", "CHTR"             # Communication
         ]
-        actions=[]
-        for i in range(n):
-            actions.append(base_de_donnee_actions[np.random.randint(0,len(base_de_donnee_actions))])
-            base_de_donnee_actions.remove(actions[i])
-        return actions
-    
+        tickers=[]
+        for i in range(n_tickers):
+            tickers.append(tickers_database[np.random.randint(0,len(tickers_database))])
+            tickers_database.remove(tickers[i])            
     model ="open-mistral-nemo-2407"
-    client=MistralClient(api_key=api_key)
-    request="donne moi au format JSON un objet qui contient uniquement une liste de "+ str(n) +" tickers valide sur Yahoo Finance, d'entreprises françaises et internationales dans des secteurs d'activité varié, qui ont performées entre " + start + " et " + end
-    resp=client.chat(model=model,messages=[ChatMessage(role="system",content=prefixe),ChatMessage(role="user",content=request),])
+    client=Mistral(api_key=api_key)
     
-    actions_str=resp.choices[0].message.content
-    #actions = json.loads(actions_str) parfois actions_str est déjà une liste, souvent un dict
-    actions_dict = json.loads(actions_str)
-    actions = actions_dict['tickers']
-    if (type(actions))!=list:
-        print("Erreur lors de la récupération des tickers")
+    #the prompt :
+    messages=[
+        {
+            "role": "user",
+            "content": "donne moi au format JSON une liste de "+ str(n_tickers) +
+            " tickers valide sur Yahoo Finance, d'entreprises françaises, européennes et internationales dans des secteurs d'activité varié, qui ont performées entre " 
+            + start + " et " + end + 
+            ". Tu peux aussi donner des cryptomonnaies mais pas trop ! "
+            "Attention à donner des tickers valide (par exemple 'FB' n'existe pas) car certains ont récemment changer de nom et surtout donne juste le nom des tickers dans une liste, sans rien ajouter",
+        }
+    ]
+    response=client.chat.complete(model=model,messages=messages,response_format ={"type": "json_object",})
+
+    tickers_str=response.choices[0].message.content
+    tickers = json.loads(tickers_str)
+    print("\nMistral proposition :\n",tickers,"\n\n")
+    #tickers=tickers_str["tickers"]
+    if (type(tickers))!=list:
+        print("Error during tickers recuperation")
         return []
-    return actions
+    
+    returns=yf.Tickers(tickers).history(start=start,end=end).pct_change(fill_method=None)['Close']
+    #Sometimes MistralAI goes crazy 
+    valid_tickers=[]
+    for ticker in tickers:
+        if ticker not in returns.columns: 
+            print(f"\nError during the download of {ticker} (It might not exist)\n")
+        elif returns[ticker].isna().all():
+            print(f"\nError during the download of {ticker} (It might not exist)\n")
+        else:
+            valid_tickers.append(ticker)
+    return valid_tickers
 
-def affichage_meilleure_partition(actions,start,end,nombre_portefeuilles):
-    meilleure_partition,rendements=meilleure_combinaison(actions,start,end)
-    meilleure_partition = [round(x,3) for x in meilleure_partition]#arrondie a 3 chiffres apres la virgule
-    actions_a_garder = [(actions[i], meilleure_partition[i]) for i in range(len(meilleure_partition)) if meilleure_partition[i] > 0]
-    action_a_jetee = [actions[i] for i in range(len(meilleure_partition)) if meilleure_partition[i] == 0]
-    for action, poids in actions_a_garder:
-        print(f'Action: {action}, Poids: {poids*100}%')
-
-    #print("Combinaison optimale :", actions_a_garder)
-
-    meilleur_couple=[calcul_rendement_portefeuille(meilleure_partition,rendements.mean()),calcul_risque_portefeuille(meilleure_partition,rendements.cov())]
-    print("Actions à enlever :",action_a_jetee)
-    print(f'Ce portefeuille aurait eu un rendement de {round(meilleur_couple[0]*100,4)}%, et un risque de {round(meilleur_couple[1]*100,4)}% entre {start} et {end}')
-    couples = generer_portefeuilles(rendements, nombre_portefeuilles)
-    afficher_portefeuilles(couples,meilleur_couple)
+def backtest(weights:list,tickers:list,expected_return,test_start,test_end):
+    returns=yf.Tickers(tickers).history(start=test_start,end=test_end)['Close']
+    returns.index=pd.to_datetime(returns.index)#convert returns' index into a Datetime
+    backtest=100*returns/returns.iloc[0] #normalize
+    backtest=backtest.ffill() #different timezone leads to missing data
+    weights=np.array(weights)
+    #print(backtest)
+    backtest["Portfolio Close"]=(weights*backtest).sum(axis=1)
+    print("Return of the portfolio on the tested period : \n", round(100*(backtest["Portfolio Close"].iloc[-1]/100-1),3),"%")
+    expected_return_over_period=((1+expected_return)**(returns.shape[0])-1)*100
+    print("Expected return of the portfolio over the period :",round(expected_return_over_period,3),"%")
+    ax=backtest.drop(columns="Portfolio Close").plot(title="Portfolio Backtesting")
+    backtest["Portfolio Close"].plot(ax=ax,color="black", label="Portfolio Strike")
+    plt.show()
     return
 
+n=10000
 start = '2024-01-01'#yyyy-mm-dd
-end = '2024-08-07'
-n=10
-nombre_portefeuilles=10000
+end = '2024-08-01'
+test_start=end
+test_end='2024-12-01'
 
-actions = tickers_mistral(n,start,end)
-affichage_meilleure_partition(actions,start,end,nombre_portefeuilles)
+tickers=mistral_tickers_selection(5)
+rounded_opti_set,expected_return=global_print(tickers,start,end,n)
+backtest(rounded_opti_set,tickers,expected_return,test_start,test_end)
