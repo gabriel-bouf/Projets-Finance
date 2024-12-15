a4df44ca71c32e2439e5ed2144a37c66a3ae69ff
Gabriel Bouf <165051839+gabriel-bouf@users.noreply.github.com>
2024-12-15 15:55:49 +0100

    Update Projet-1.py

--- Projet-1.py
+++ Projet-1.py
@@ -4,56 +4,27 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 from math import sqrt
 
+def index_SP_avec_risque(ticker:str,periode:str,risk:int,fenetre:int,lag:int,EWA:int,plot:bool):
+    stock=yf.Ticker(ticker)
+    history=stock.history(periode)
+    returns=history.pct_change()["Close"]
+    returns=returns.to_frame(name="Returns")
+    volatility=returns.rolling(fenetre).std().ffill() # si fenetre=30 rolling calcul avec la 30ieme pour le j30
+    returns.insert(0,"Close",history["Close"])
+    returns.insert(2,"Volatility",volatility)
+    #returns.insert(3,"Annualized returns",(1+returns["Returns"])**252-1)
+    returns.insert(3,"Annualized std",returns["Volatility"]*sqrt(252))
+    returns.insert(4,"Controled returns",returns["Close"])
 
-def index_SP_avec_risque(nom:str,periode:str,risk:int,fenetre:int):
+    returns["Controled returns"]=returns.apply(lambda row: row["Returns"]*((risk*0.01)/row["Annualized std"]) if row["Annualized std"]>(risk*0.01) else row["Returns"],axis=1)
+    returns.insert(5,"Controled returns STD",sqrt(252)*returns["Controled returns"].rolling(fenetre).std().ffill())
+    if plot:
+        returns.insert(5,"Strike",100*((1+returns["Returns"].fillna(0)).cumprod()))
+        returns.insert(6,"Controled Strike",100*((1+returns["Controled returns"].fillna(0)).cumprod()))
+        returns["Strike"].plot()
+        returns["Controled Strike"].plot()
+        plt.show()
+    else:
+        print(returns)
 
-    historique = yf.Ticker(nom).history(period=periode)
-    n=len(historique.index)
-    if n<fenetre+10:
-        print("FENETRE TROP GRANDE")
-    
-    rendement=[0]*n
-    vol=[0.1]*n
-    rendement_ajuste=[0]*n
-    prix_ajuste=[historique.Close[0]]*n
-
-    #calcul du rendement à la main
-    for i in range(1,n):
-        rendement[i]= (historique.Close[i]-historique.Close[i-1])/historique.Close[i-1]
-
-    #calcul de la vol sur fenetre jours, les fenetre premières case de vol[] seront donc à 0.1
-    for i in range(fenetre-1,n):
-        rendement_moy=np.mean(rendement[i-fenetre+1:i+1])
-        variance=0
-        for j in range(fenetre):
-            variance+=(rendement[i-j]-rendement_moy)**2
-        variance/=(fenetre-1)
-        vol[i]= sqrt(variance)*sqrt(252)#vol annuelle, il faut multiplier par 252, le nombre de période 
-
-    #calcul du prix ajusté
-    for i in range(n):#la vol est nulle avant fenetre
-        if vol[i]!=0:
-            rendement_ajuste[i]=rendement[i]*(risk/(100*vol[i]))
-            prix_ajuste[i]=prix_ajuste[i-1]*(1+rendement_ajuste[i])
-
-    #normalisation
-    normalisation=historique.Close[0]/100
-    for i in range(n):
-        prix_ajuste[i]/=normalisation
-        historique.Close[i]/=normalisation
-    
-
-    plt.figure(figsize=(10, 6))
-    plt.plot(historique.index, historique.Close, label=nom)
-    plt.plot(historique.index,prix_ajuste,label="prix ajuste du " + nom)
-
-    plt.title("Historique de "+nom+" par rapport a celui d'un risque de "+ str(risk) +"%")
-    plt.xlabel("Date")
-    plt.ylabel("Prix de clôture")
-    plt.legend()
-    plt.show()
-    return
-
-index_SP_avec_risque("^GSPC","1y",5,30)
-#index_SP_avec_risque("AAPL","1y",10,30)
-
+index_SP_avec_risque("^GSPC","1y",10,5,1,10,plot=True)
