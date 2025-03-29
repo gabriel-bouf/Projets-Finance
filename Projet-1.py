6f39e865c439b8cdbf3e6561939affd604c90d94
Gabriel Bouf <165051839+gabriel-bouf@users.noreply.github.com>
2025-03-29 11:04:02 +0100

    Update Projet-1.py

--- Projet-1.py
+++ Projet-1.py
@@ -11,12 +11,12 @@ def index_SP_avec_risque(ticker:str,periode:str,risk:int,fenetre:int,lag:int,EWA
     returns=returns.to_frame(name="Returns")
     volatility=returns.rolling(fenetre).std().ffill() # si fenetre=30 rolling calcul avec la 30ieme pour le j30
     returns.insert(0,"Close",history["Close"])
-    returns.insert(2,"Volatility",volatility)
+    returns.insert(2,"Daily Volatility",volatility)
     #returns.insert(3,"Annualized returns",(1+returns["Returns"])**252-1)
-    returns.insert(3,"Annualized std",returns["Volatility"]*sqrt(252))
+    returns.insert(3,"Annualized Volatility",returns["Daily Volatility"]*sqrt(252))
     returns.insert(4,"Controled returns",returns["Close"])
 
-    returns["Controled returns"]=returns.apply(lambda row: row["Returns"]*((risk*0.01)/row["Annualized std"]) if row["Annualized std"]>(risk*0.01) else row["Returns"],axis=1)
+    returns["Controled returns"]=returns.apply(lambda row: row["Returns"]*((risk*0.01)/row["Annualized Volatility"]) if row["Annualized Volatility"]>(risk*0.01) else row["Returns"],axis=1)
     returns.insert(5,"Controled returns STD",sqrt(252)*returns["Controled returns"].rolling(fenetre).std().ffill())
     if plot:
         returns.insert(5,"Strike",100*((1+returns["Returns"].fillna(0)).cumprod()))
