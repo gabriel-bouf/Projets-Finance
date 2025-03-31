e1be7c9935f1b0adf6b123e09059edd01ac99cf9
Gabriel Bouf <165051839+gabriel-bouf@users.noreply.github.com>
2025-04-01 00:02:24 +0200

    Update Projet-1.py

--- Projet-1.py
+++ Projet-1.py
@@ -2,29 +2,49 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
-from math import sqrt
 
-def index_SP_avec_risque(ticker:str,periode:str,risk:int,fenetre:int,lag:int,EWA:int,plot:bool):
+def ewa_volatility(returns, EWA):
+    """mean_returns = returns.ewm(alpha=EWA).mean()
+    var_ewa = (returns - mean_returns) ** 2
+    vol_ewa = np.sqrt(var_ewa.ewm(alpha=EWA).mean()) * np.sqrt(252)"""
+
+    r=returns**2
+    vol_ewa=np.sqrt(r.ewm(alpha=EWA).mean()*252)
+    return vol_ewa
+
+
+def index_SP_avec_risque(ticker:str,period:str,risk:int,EWA:float,plot:bool):
+    risk_free_rate=0.043
+    daily_risk_free_rate=np.exp(np.log(1+risk_free_rate)/252)-1
     stock=yf.Ticker(ticker)
-    history=stock.history(periode)
+    history=stock.history(period)
     returns=history.pct_change()["Close"]
-    returns=returns.to_frame(name="Returns")
-    volatility=returns.rolling(fenetre).std().ffill() # si fenetre=30 rolling calcul avec la 30ieme pour le j30
-    returns.insert(0,"Close",history["Close"])
-    returns.insert(2,"Daily Volatility",volatility)
-    #returns.insert(3,"Annualized returns",(1+returns["Returns"])**252-1)
-    returns.insert(3,"Annualized Volatility",returns["Daily Volatility"]*sqrt(252))
-    returns.insert(4,"Controled returns",returns["Close"])
+    
+    df=returns.to_frame(name="Returns")
+    df.insert(0, "Close", history["Close"])
 
-    returns["Controled returns"]=returns.apply(lambda row: row["Returns"]*((risk*0.01)/row["Annualized Volatility"]) if row["Annualized Volatility"]>(risk*0.01) else row["Returns"],axis=1)
-    returns.insert(5,"Controled returns STD",sqrt(252)*returns["Controled returns"].rolling(fenetre).std().ffill())
+    df["Volatility(per year)"]=ewa_volatility(df["Returns"],EWA=EWA)             ####returns.rolling(window).std().ffill()*np.sqrt(252)
+    df["Controled returns"]=df.apply(lambda row: min(1,(risk*0.01)/row["Volatility(per year)"])*row["Returns"]+(1-min(1,(risk*0.01)/row["Volatility(per year)"]))*daily_risk_free_rate , axis=1)#if row["Annualized std"]>1e-4 else row["Returns"]
+    df["Controled Volatility"]=ewa_volatility(df["Controled returns"],EWA=EWA)
+    df["Spot"]=100*((1+df["Returns"].fillna(0)).cumprod())
+    df["Controled Spot"]=100*((1+df["Controled returns"].fillna(0)).cumprod())
+
+    df["Cash returns"]=1+daily_risk_free_rate
+    df["Cash returns"]=100*(df["Cash returns"].cumprod())
+
     if plot:
-        returns.insert(5,"Strike",100*((1+returns["Returns"].fillna(0)).cumprod()))
-        returns.insert(6,"Controled Strike",100*((1+returns["Controled returns"].fillna(0)).cumprod()))
-        returns["Strike"].plot()
-        returns["Controled Strike"].plot()
-        plt.show()
-    else:
-        print(returns)
+        df["Spot"].plot()
+        df["Controled Spot"].plot()
+        df["Cash returns"].plot()
 
-index_SP_avec_risque("^GSPC","1y",10,5,1,10,plot=True)
+        plt.figure()
+        
+        df["Volatility(per year)"].plot()
+        df["Controled Volatility"].plot()
+        plt.show()
+        
+        print(df)
+    else:
+        print(df)
+
+index_SP_avec_risque("^GSPC","6mo",10,20,0.4,plot=True)
